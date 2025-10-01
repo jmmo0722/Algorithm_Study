a274611cb45c6c838e97268ec7038eb9d7c351f0
jmmo0722 <moguricamera@gmail.com>
2025-10-01 22:29:24 +0900

    docs: 지름길.md 작성

---
+++ src/main/java/Baekjoon/Silver/지름길_1446/지름길.java
@@ -0,0 +1,20 @@
+package Baekjoon.Silver.지름길_1446;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+public class 지름길 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer str;
+
+
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
